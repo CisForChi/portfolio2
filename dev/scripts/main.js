383022321c567343525b7a54e8d51cd6615528b1
Chiara Zielinsky <chiara.zielinsky@gmail.com>
2018-03-31 11:36:29 -0400

    header, imgs

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,38 @@
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$.fn.randomOrder = function(animate) {
+  this.each(function() {
+    var image = $(this);
+    
+    // Viewport Dimensions
+    var vpHeight = $(window).height();
+    var vpWidth = $(window).width();
+    
+    // Image Position
+    var xPos = getRandomInt(0, vpWidth - image.width());
+    var yPos = getRandomInt(0, vpHeight - image.height());
+    var zIndex = getRandomInt(0,13);
+    
+    // Animation Duration
+    if(animate) var dur = 500;
+    else var dur = 0;
+    
+    image.animate({left: xPos, top: yPos, 'z-index': zIndex}, dur);
+  });
+};
+
+//Setup
+$('img').randomOrder(false);
+$('img').draggable({stack: "img"});
+
+// Change after 10 Seconds
+window.setInterval(function(){
+  $('img').randomOrder(true);
+}, 10000);
+
 const ohHey = "Hello World";
 
 console.log(ohHey);
@@ -7,3 +42,22 @@ $(".box-info").hover(function(){
 },function(){
     $('.hover-info').hide();
 });
+
+(function($){
+			// variables
+			elementWidth = $('ul').width(),
+			containerWidth = $('nav').width(),
+			difference = elementWidth-containerWidth,
+			finalWidth = difference * 1.5,
+			element = $('ul');
+			
+			// active on click
+			$('li').on('click', function(){
+			  $('li').removeClass('active');
+			  $(this).addClass('active');
+			});
+			
+		  })(jQuery);
+		  
+		  
+    
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,37 @@
-"use strict";
+'use strict';
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$.fn.randomOrder = function (animate) {
+    this.each(function () {
+        var image = $(this);
+
+        // Viewport Dimensions
+        var vpHeight = $(window).height();
+        var vpWidth = $(window).width();
+
+        // Image Position
+        var xPos = getRandomInt(0, vpWidth - image.width());
+        var yPos = getRandomInt(0, vpHeight - image.height());
+        var zIndex = getRandomInt(0, 13);
+
+        // Animation Duration
+        if (animate) var dur = 500;else var dur = 0;
+
+        image.animate({ left: xPos, top: yPos, 'z-index': zIndex }, dur);
+    });
+};
+
+//Setup
+$('img').randomOrder(false);
+$('img').draggable({ stack: "img" });
+
+// Change after 10 Seconds
+window.setInterval(function () {
+    $('img').randomOrder(true);
+}, 10000);
 
 var ohHey = "Hello World";
 
@@ -9,3 +42,14 @@ $(".box-info").hover(function () {
 }, function () {
     $('.hover-info').hide();
 });
+
+(function ($) {
+    // variables
+    elementWidth = $('ul').width(), containerWidth = $('nav').width(), difference = elementWidth - containerWidth, finalWidth = difference * 1.5, element = $('ul');
+
+    // active on click
+    $('li').on('click', function () {
+        $('li').removeClass('active');
+        $(this).addClass('active');
+    });
+})(jQuery);
