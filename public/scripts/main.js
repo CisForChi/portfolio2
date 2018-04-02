8d5eb1cd864137583c9cdeb1fda8692317354acf
Chiara Zielinsky <chiara.zielinsky@gmail.com>
2018-04-02 09:06:44 -0400

    beginning of responsive design

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 $.fn.randomOrder = function (animate) {
-    this.each(function () {
-        var image = $(this);
+  this.each(function () {
+    var image = $(this);
 
-        // Viewport Dimensions
-        var vpHeight = $(window).height();
-        var vpWidth = $(window).width();
+    // Viewport Dimensions
+    var vpHeight = $(window).height();
+    var vpWidth = $(window).width();
 
-        // Image Position
-        var xPos = getRandomInt(0, vpWidth - image.width());
-        var yPos = getRandomInt(0, vpHeight - image.height());
-        var zIndex = getRandomInt(0, 13);
+    // Image Position
+    var xPos = getRandomInt(0, vpWidth - image.width());
+    var yPos = getRandomInt(0, vpHeight - image.height());
+    var zIndex = getRandomInt(0, 13);
 
-        // Animation Duration
-        if (animate) var dur = 500;else var dur = 0;
+    // Animation Duration
+    if (animate) var dur = 500;else var dur = 0;
 
-        image.animate({ left: xPos, top: yPos, 'z-index': zIndex }, dur);
-    });
+    image.animate({ left: xPos, top: yPos, 'z-index': zIndex }, dur);
+  });
 };
 
 //Setup
@@ -30,7 +30,7 @@ $('img').draggable({ stack: "img" });
 
 // Change after 10 Seconds
 window.setInterval(function () {
-    $('img').randomOrder(true);
+  $('img').randomOrder(true);
 }, 10000);
 
 var ohHey = "Hello World";
@@ -38,18 +38,18 @@ var ohHey = "Hello World";
 console.log(ohHey);
 
 $(".box-info").hover(function () {
-    $('.hover-info').show();
+  $('.hover-info').show();
 }, function () {
-    $('.hover-info').hide();
+  $('.hover-info').hide();
 });
 
 (function ($) {
-    // variables
-    elementWidth = $('ul').width(), containerWidth = $('nav').width(), difference = elementWidth - containerWidth, finalWidth = difference * 1.5, element = $('ul');
+  // variables
+  elementWidth = $('ul').width(), containerWidth = $('nav').width(), difference = elementWidth - containerWidth, finalWidth = difference * 1.5, element = $('ul');
 
-    // active on click
-    $('li').on('click', function () {
-        $('li').removeClass('active');
-        $(this).addClass('active');
-    });
+  // active on click
+  $('li').on('click', function () {
+    $('li').removeClass('active');
+    $(this).addClass('active');
+  });
 })(jQuery);
