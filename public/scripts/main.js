a4369bb6dffacbe937e2a6e25606a4075d145fdb
Chiara Zielinsky <chiara.zielinsky@gmail.com>
2018-04-15 18:57:07 -0400

    removed some JS, added CSS animation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,39 +1,5 @@
 "use strict";
 
-var ohHey = "Hello World";
+var ohHey = "Hello World! Let's connect...hi@chiaracodes.com. Currently looking for FT opportunities.";
 
 console.log(ohHey);
-
-$(".box-info").hover(function () {
-				$('.hover-info').show();
-}, function () {
-				$('.hover-info').hide();
-});
-
-(function ($) {
-				// variables
-				elementWidth = $('ul').width(), containerWidth = $('nav').width(), difference = elementWidth - containerWidth, finalWidth = difference * 1.5, element = $('ul');
-
-				// active on click
-				$('li').on('click', function () {
-								$('li').removeClass('active');
-								$(this).addClass('active');
-				});
-})(jQuery);
-
-//jQuery is required to run this code
-$(document).ready(function () {
-
-				scaleVideoContainer();
-
-				initBannerVideoSize('.video-container .poster img');
-				initBannerVideoSize('.video-container .filter');
-				initBannerVideoSize('.video-container video');
-
-				$(window).on('resize', function () {
-								scaleVideoContainer();
-								scaleBannerVideoSize('.video-container .poster img');
-								scaleBannerVideoSize('.video-container .filter');
-								scaleBannerVideoSize('.video-container video');
-				});
-});
